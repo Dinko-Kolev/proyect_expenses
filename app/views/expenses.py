a9d6aa515963a00e04b6c4cdf6de91d2908879e4
Dinko-Kolev <dinkonikolaevkolev@gmail.com>
2021-01-06 23:46:45 +0100

    Added Expenses edit and delete functions

--- app/views/expenses.py
+++ app/views/expenses.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from app.forms.expenses import ExpensesForm
 from django.shortcuts import redirect
 
-
 # Create your view here.
+from app.models import Expense
 
 
 def create_expense(request):
@@ -24,9 +24,45 @@ def create_expense(request):
         return render(request, 'expense-create.html', context)
 
 
-def edit_expense(request):
-    return render(request, 'expense-edit.html')
+def edit_expense(request, pk):
+    expense = Expense.objects.get(pk=pk)
+    if request.method == 'GET':
+        context = {
+            'expense': expense,
+            'form': ExpensesForm(instance=expense),
+        }
+        return render(request, 'expense-edit.html', context)
+
+    else:
+        form = ExpensesForm(request.POST, instance=expense)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+
+        context = {
+            'expense': expense,
+            'form': form,
+        }
+        return render(request, 'expense-edit.html', context)
 
 
-def delete_expense(request):
-    return render(request, 'expense-delete.html')
+def delete_expense(request, pk):
+    expense = Expense.objects.get(pk=pk)
+    if request.method == 'GET':
+        context = {
+            'expense': expense,
+            'form': ExpensesForm(instance=expense),
+        }
+        return render(request, 'expense-delete.html', context)
+
+    else:
+        form = ExpensesForm(request.POST, instance=expense)
+        if form.is_valid():
+            expense.delete()
+            return redirect('index')
+
+        context = {
+            'expense': expense,
+            'form': form,
+        }
+        return render(request, 'expense-delete.html', context)
